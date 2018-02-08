01e630c2f4d5d5d20ab9b937bc9dd3e8ffef2820
Radoslaw Podgorski <radoslaw.podgorski@gmail.com>
2018-02-08 11:51:47 +0000

    fix error & finish

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,10 @@
 
 var getButtonID = document.getElementById('btn'),
 	getButtonClass = document.getElementsByClassName('button'),
-	numberOfChildren = getButtonClass.length,
-	buttonText = getButtonID.innerText,
-	i = 1;
+	numberOfChildren = getButtonClass.length; 
 
 console.log('Number of buttons: ' + numberOfChildren);
 
-for (i = 1; i <= numberOfChildren; i++) {
-	console.log(buttonText.childNodes[i]);
+for (var i = 0; i < numberOfChildren; i++) {
+	console.log(getButtonClass[i].innerText);
 }
